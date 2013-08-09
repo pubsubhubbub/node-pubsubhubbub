8ac6a4be0c8bc0054d1ab3fc754dc3b5b518ecb9
Andris Reinman <andris@kreata.ee>
2013-08-09 14:19:17 +0300

    Updated API to be 0.4 hubs compatible, bumped version

--- examples/example.js
+++ examples/example.js
@@ -1,46 +1,48 @@
-var PubSubHubbub = require("../index").PubSubHubbub;
+var pubSubHubbub = require("../index"),
+    crypto = require("crypto"),
+    
+    pubsub = pubSubHubbub.createServer({
+        callbackUrl: "http://kreata.ee:1337",
+        secret: "MyTopSecret"
+    }),
+    
+    topic = "http://testetstetss.blogspot.com/feeds/posts/default",
+    hub = "http://pubsubhubbub.appspot.com/";
 
-var pubsub = new PubSubHubbub({
-    callbackServer: "http://subscriber.node.ee",
-    port: 8922
+pubsub.listen(1337);
+
+pubsub.on("denied", function(data){
+    console.log("Denied");
+    console.log(data);
 });
 
 pubsub.on("subscribe", function(data){
     console.log("Subscribe");
     console.log(data);
+
+    console.log("Subscribed "+topic+" to "+hub);
 });
 
 pubsub.on("unsubscribe", function(data){
     console.log("Unsubscribe");
     console.log(data);
+
+    console.log("Unsubscribed "+topic+" from "+hub);
 });
 
 pubsub.on("error", function(error){
     console.log("Error");
-    console.log(data);
+    console.log(error);
 });
 
-pubsub.on("feed", function(feed){
-    console.log(feed);
-    pubsub.unsubscribe(feed.getPermalink(), feed.getHub(), console.log.bind(console, "Unsubscribed "+feed.getPermalink()+" from "+feed.getHub()));
+pubsub.on("feed", function(data){
+    console.log(data)
+    console.log(data.feed.toString());
+
+    pubsub.unsubscribe(topic, hub);
 });
 
 pubsub.on("listen", function(){
-    var topic = "http://testetstetss.blogspot.com/feeds/posts/default",
-        hub = "http://pubsubhubbub.appspot.com/";
-
-    pubsub.subscribe(topic, hub, function(err, subscription){
-        if(err){
-            console.log("Subscribing failed");
-            console.log(err);
-            return;
-        }
-
-        if(subscription == topic){
-            console.log("Subscribed "+topic+" to "+hub);
-        }else{
-            console.log("Invalid response");
-            return;
-        }
-    });
+    console.log("Server listening on port %s", pubsub.port);
+    pubsub.subscribe(topic, hub);
 });
